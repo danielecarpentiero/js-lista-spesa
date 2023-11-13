e3a99a26594063a9d05a88aeef758666760e6f81
danielecarpentiero <carpentierodaniele@gmail.com>
2023-11-13 14:57:29 +0100

    feat: risoluzione esercizio (console & HTML)

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# js-lista-spesa
+# Lista della spesa con while / do-while
+
+1. dichiaro un array _list_, che contiene tutti gli elementi della lista della spesa
+2. dichiaro una variabile _i_, che è il mio singolo elemento
+3. dichiaro una variabile che registra in memoria una lista dall'HTML
+4. creo il ciclo _while_:
+   - mi creo una lista e la "appendo" all'ul padre
+   - in console o nell'HTML appare ogni elemento dell'array "ciclato"
+   - passo all'elemento successivo finché gli elementi non sono terminati
---
+++ main.js
@@ -0,0 +1,25 @@
+/* creazione array lista della spesa */
+const list = [
+  "Pane",
+  "Pasta",
+  "Uova",
+  "Insalata",
+  "Frutta",
+  "Merendine",
+  "Formaggio",
+  "Prosciutto",
+  "Caffé",
+];
+
+/* inizializzazione singolo elemento */
+let i = 0;
+/* seleziono elemento nella lista */
+const ulElement = document.querySelector(".list");
+/* ciclo while */
+while (i < list.length) {
+  console.log(list[i]);
+  const listItem = document.createElement("li");
+  listItem.innerHTML = list[i];
+  ulElement.appendChild(listItem);
+  i++;
+}
