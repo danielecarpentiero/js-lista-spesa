815f644118be0f03346498abff4ace5d30411382
danielecarpentiero <carpentierodaniele@gmail.com>
2023-11-13 15:00:57 +0100

    fix: append

--- main.js
+++ main.js
@@ -20,6 +20,6 @@ while (i < list.length) {
   console.log(list[i]);
   const listItem = document.createElement("li");
   listItem.innerHTML = list[i];
-  ulElement.appendChild(listItem);
+  ulElement.append(listItem);
   i++;
 }
